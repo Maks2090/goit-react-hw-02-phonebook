a905529906edd573d74d3c849e5c739b9df1c336
Maksym Chyshko <76655891+Maks2090@users.noreply.github.com>
2021-09-08 22:46:25 +0300

    start files

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+
+
+class App extends React.Component {
+  state = {
+    contacts: [],
+    name: ''
+  }
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default App;
